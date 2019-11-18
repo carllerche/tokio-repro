e0b199a891441a50c0e00f6519a7b0acfab825e4
Carl Lerche <me@carllerche.com>
2019-11-18 09:24:15 -0800

    switch to seqcst potential fix

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 fn main() {
-    const NR_ITERATIONS: usize = 1_000_000;
+    const NUM_ITERATIONS: usize = 1_000_000;
+    const NUM_THREADS: usize = 2;
 
-    let mut rt = tokio::runtime::Builder::new()
-        .threaded_scheduler()
-        .num_threads(1)
-        .build()
-        .unwrap();
+    for i in 0.. {
+        let mut rt = tokio::runtime::Builder::new()
+            .threaded_scheduler()
+            .num_threads(NUM_THREADS)
+            .build()
+            .unwrap();
 
-    for i in 0 .. NR_ITERATIONS {
-        if i % 1_000 == 0 { println!(" + {}", i) }
+        for j in 0 .. NUM_ITERATIONS {
+            if j % 1_000 == 0 { println!(" + {} - {}", i, j) }
 
-        rt.block_on(async {
-            let handle = tokio::spawn(async {});
-            handle.await.unwrap();
-        });
+            rt.block_on(async {
+                let handle = tokio::spawn(async {});
+                handle.await.unwrap();
+            });
+        }
     }
 }
